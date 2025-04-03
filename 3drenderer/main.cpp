b8d2209a7932752dbf8ac0381d87476e5c6206cc
standardversion <standardversion@tuta.io>
2025-04-03 10:33:07 -0700

    moves graphics pipeline in a separate func

--- 3drenderer/main.cpp
+++ 3drenderer/main.cpp
@@ -18,6 +18,188 @@
 #include "clipping.h"
 #include "polygon.h"
 
+
+///////////////////////////////////////////////////////////////////////////////
+// Process the graphics pipeline stages for all the mesh triangles
+///////////////////////////////////////////////////////////////////////////////
+// +-------------+
+// | Model space |  <-- original mesh vertices
+// +-------------+
+// |   +-------------+
+// `-> | World space |  <-- multiply by world matrix
+//     +-------------+
+//     |   +--------------+
+//     `-> | Camera space |  <-- multiply by view matrix
+//         +--------------+
+//         |    +------------+
+//         `--> |  Clipping  |  <-- clip against the six frustum planes
+//              +------------+
+//              |    +------------+
+//              `--> | Projection |  <-- multiply by projection matrix
+//                   +------------+
+//                   |    +-------------+
+//                   `--> | Image space |  <-- apply perspective divide
+//                        +-------------+
+//                        |    +--------------+
+//                        `--> | Screen space |  <-- ready to render
+//                             +--------------+
+///////////////////////////////////////////////////////////////////////////////
+void process_graphics_pipeline(
+	geo::Mesh& mesh_to_render,
+	const matrix::Matrix4x4& projection_matrix,
+	std::vector<geo::Triangle<int>>& triangles_to_render,
+	camera::camera_t& view_camera,
+	std::vector<clip::plane_t>& clipping_planes,
+	const bool backface_culling,
+	const shading::Light& light,
+	const bool render_face_center,
+	const bool render_normals
+)
+{
+	vector::Vector3d origin{ 0.0, 0.0, 0.0 };
+	vector::Vector3d up{ 0.0, 1.0, 0.0 };
+	vector::Vector3d target{ 0.0, 0.0, 1.0 };
+	matrix::Matrix4x4 camera_yaw_rotation{ matrix::Matrix4x4::make_rotation_matrix(view_camera.m_yaw, 'y') };
+	matrix::Matrix4x4 camera_pitch_rotation{ matrix::Matrix4x4::make_rotation_matrix(view_camera.m_pitch, 'x') };
+	matrix::Matrix4x4 camera_rotation{};
+	camera_rotation *= camera_pitch_rotation;
+	camera_rotation *= camera_yaw_rotation;
+	view_camera.m_direction = camera_rotation.mult_vec4d(target);
+
+	//offset the camera position in the direction where the camera is pointing at
+	target = view_camera.m_position + view_camera.m_direction;
+	// create the view matrix looking at a hardcoded targetpoint
+	matrix::Matrix4x4 view_matrix{ matrix::Matrix4x4::make_view_matrix(view_camera.m_position, target, up) };
+
+	matrix::Matrix4x4 scale_matrix{ matrix::Matrix4x4::make_scale_matrix(mesh_to_render.m_scale.m_x, mesh_to_render.m_scale.m_y, mesh_to_render.m_scale.m_z) };
+	matrix::Matrix4x4 translation_matrix{ matrix::Matrix4x4::make_translation_matrix(mesh_to_render.m_translation.m_x, mesh_to_render.m_translation.m_y, mesh_to_render.m_translation.m_z) };
+	matrix::Matrix4x4 rotatation_matrix_x{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_x, 'x') };
+	matrix::Matrix4x4 rotatation_matrix_y{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_y, 'y') };
+	matrix::Matrix4x4 rotatation_matrix_z{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_z, 'z') };
+
+	for (const auto& face : mesh_to_render.m_faces)
+	{
+		//each mesh face has 3 vertices stored as the index of the vertex in mesh_vertices
+		//face_vertices is an array of 3 vector3d  containing x,y,z coordinates of each vertex
+		std::vector<vector::Vector3d> face_vertices{
+			mesh_to_render.m_vertices[face.a - 1],
+			mesh_to_render.m_vertices[face.b - 1],
+			mesh_to_render.m_vertices[face.c - 1]
+		};
+		// normals
+		std::vector<vector::Vector3d> face_vtx_normals{
+			mesh_to_render.m_vertex_normals[face.a - 1],
+			mesh_to_render.m_vertex_normals[face.b - 1],
+			mesh_to_render.m_vertex_normals[face.c - 1]
+
+		};
+
+		std::vector<vector::Vector4d> transformed_vertices{};
+		std::vector<vector::Vector3d> transformed_normals{};
+		// apply transformations
+		std::size_t counter{ 0 };
+		for (const auto& vertex : face_vertices)
+		{
+			vector::Vector4d point{ vertex };
+			matrix::Matrix4x4 world_matrix{};
+			// order matters scale, then rotate and then translate
+			world_matrix *= scale_matrix;
+			world_matrix *= rotatation_matrix_z;
+			world_matrix *= rotatation_matrix_y;
+			world_matrix *= rotatation_matrix_x;
+			world_matrix *= translation_matrix;
+			point = world_matrix.mult_vec4d(point);
+			point = view_matrix.mult_vec4d(point);
+			transformed_vertices.push_back(point);
+
+			vector::Vector4d transformed_normal{ face_vtx_normals[counter] };
+			transformed_normal = world_matrix.get_inverse().get_transpose().mult_vec4d(transformed_normal);
+			transformed_normal = view_matrix.get_inverse().get_transpose().mult_vec4d(transformed_normal);
+			vector::Vector3d transformed_normal_vec3{ transformed_normal };
+			transformed_normal_vec3.normalize();
+			transformed_normals.emplace_back(transformed_normal_vec3);
+			counter++;
+		}
+
+		std::vector<vector::Vector3d> per_vertex_normals{ mesh_to_render.get_per_vertex_normals(transformed_vertices) };
+		if (backface_culling) {
+			// find the camera ray ie the vector between a point in the triangle and the camera origin
+			vector::Vector3d camera_ray{ origin - transformed_vertices[0] };
+
+			// calculate how aligned the camera ray is with the face normal (using dot product)
+			if (camera_ray.dot_product(per_vertex_normals[0]) < 0)
+			{
+				continue;
+			}
+		}
+
+		//clipping
+		//loop through each vertex and project the points of those faces
+		// convert the triangle points to ints as we'll project to screen space so we're dealing with x & y pixels which are in ints
+
+		// uvs don't need to be transformed
+		std::vector<vector::Vector2d<double>> face_vtx_uvs{
+			mesh_to_render.m_uvs[face.a_uv - 1],
+			mesh_to_render.m_uvs[face.b_uv - 1],
+			mesh_to_render.m_uvs[face.c_uv - 1]
+		};
+
+		geo::Polygon poly{ transformed_vertices, transformed_normals, face_vtx_uvs };
+		poly.clip(clipping_planes);
+		std::vector<std::vector<vector::Vector4d>> clipped_vertices_array{};
+		std::vector<std::vector<vector::Vector2d<double>>> clipped_uvs_array{};
+		std::vector<std::vector<vector::Vector3d>> clipped_normals_array{};
+		poly.split_into_tris(clipped_vertices_array, clipped_uvs_array, clipped_normals_array);
+
+		std::size_t clipped_entities_counter{ 0 };
+		for (const auto& clipped_vertex_array : clipped_vertices_array)
+		{
+			geo::Triangle<int> triangle_to_render{ };
+			double light_intensity{ -per_vertex_normals[0].dot_product(light.m_direction) };
+			triangle_to_render.m_light_intensity = light_intensity;
+			triangle_to_render.m_surface = mesh_to_render.m_surface;
+			// project the triangle
+			counter = 0;
+			for (const auto& vertex : clipped_vertex_array)
+			{
+				vector::Vector4d projected_point{ display::project_vec4d(projection_matrix, vertex) };
+				triangle_to_render.m_per_vtx_lt_intensity.push_back(-clipped_normals_array[clipped_entities_counter][counter].dot_product(light.m_direction));
+				triangle_to_render.m_points.push_back(vector::Vector2d<int>{static_cast<int>(projected_point.m_x), static_cast<int>(projected_point.m_y)});
+				triangle_to_render.m_points_z.push_back(projected_point.m_z);
+				triangle_to_render.m_points_w.push_back(projected_point.m_w);
+				triangle_to_render.m_uvs.push_back(clipped_uvs_array[clipped_entities_counter][counter]);
+				counter++;
+			}
+			if (render_face_center || render_normals)
+			{
+				vector::Vector3d face_center{ mesh_to_render.get_face_center(clipped_vertex_array) };
+				vector::Vector4d projected_center_point{ display::project_vec4d(projection_matrix, face_center) };
+
+				triangle_to_render.m_center.m_x = projected_center_point.m_x;
+				triangle_to_render.m_center.m_y = projected_center_point.m_y;
+				if (render_normals)
+				{
+					vector::Vector3d face_normal{ clipped_normals_array[clipped_entities_counter][0] };
+					// move the normal to the center of the face
+					face_normal.m_x += face_center.m_x;
+					face_normal.m_y += face_center.m_y;
+					face_normal.m_z += face_center.m_z;
+					face_normal.normalize();
+
+					vector::Vector4d projected_face_normal_point{ display::project_vec4d(projection_matrix, face_normal) };
+
+					triangle_to_render.m_face_normal.m_x = projected_face_normal_point.m_x;
+					triangle_to_render.m_face_normal.m_y = projected_face_normal_point.m_y;
+				}
+			}
+
+			triangles_to_render.push_back(triangle_to_render);
+			clipped_entities_counter++;
+		}
+	}
+	
+}
+
 void update(
 	std::vector<geo::Mesh>& meshes,
 	const matrix::Matrix4x4& projection_matrix,
@@ -58,145 +240,7 @@ void update(
 		// move pionts away from camera
 		mesh_to_render.m_translation.m_z = 5.0;
 
-		vector::Vector3d target{ 0.0, 0.0, 1.0 };
-		matrix::Matrix4x4 camera_yaw_rotation{ matrix::Matrix4x4::make_rotation_matrix(view_camera.m_yaw, 'y')};
-		matrix::Matrix4x4 camera_pitch_rotation{ matrix::Matrix4x4::make_rotation_matrix(view_camera.m_pitch, 'x') };
-		matrix::Matrix4x4 camera_rotation{};
-		camera_rotation *= camera_pitch_rotation;
-		camera_rotation *= camera_yaw_rotation;
-		view_camera.m_direction = camera_rotation.mult_vec4d(target);
-
-		//offset the camera position in the direction where the camera is pointing at
-		target = view_camera.m_position + view_camera.m_direction;
-		// create the view matrix looking at a hardcoded targetpoint
-		matrix::Matrix4x4 view_matrix{ matrix::Matrix4x4::make_view_matrix(view_camera.m_position, target, up) };
-
-		matrix::Matrix4x4 scale_matrix{ matrix::Matrix4x4::make_scale_matrix(mesh_to_render.m_scale.m_x, mesh_to_render.m_scale.m_y, mesh_to_render.m_scale.m_z) };
-		matrix::Matrix4x4 translation_matrix{ matrix::Matrix4x4::make_translation_matrix(mesh_to_render.m_translation.m_x, mesh_to_render.m_translation.m_y, mesh_to_render.m_translation.m_z) };
-		matrix::Matrix4x4 rotatation_matrix_x{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_x, 'x') };
-		matrix::Matrix4x4 rotatation_matrix_y{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_y, 'y') };
-		matrix::Matrix4x4 rotatation_matrix_z{ matrix::Matrix4x4::make_rotation_matrix(mesh_to_render.m_rotation.m_z, 'z') };
-
-		for (const auto& face : mesh_to_render.m_faces)
-		{
-			//each mesh face has 3 vertices stored as the index of the vertex in mesh_vertices
-			//face_vertices is an array of 3 vector3d  containing x,y,z coordinates of each vertex
-			std::vector<vector::Vector3d> face_vertices{
-				mesh_to_render.m_vertices[face.a - 1],
-				mesh_to_render.m_vertices[face.b - 1],
-				mesh_to_render.m_vertices[face.c - 1]
-			};
-			// normals
-			std::vector<vector::Vector3d> face_vtx_normals{
-				mesh_to_render.m_vertex_normals[face.a - 1],
-				mesh_to_render.m_vertex_normals[face.b - 1],
-				mesh_to_render.m_vertex_normals[face.c - 1]
-
-			};
-
-			std::vector<vector::Vector4d> transformed_vertices{};
-			std::vector<vector::Vector3d> transformed_normals{};
-			// apply transformations
-			std::size_t counter{ 0 };
-			for (const auto& vertex : face_vertices)
-			{
-				vector::Vector4d point{ vertex };
-				matrix::Matrix4x4 world_matrix{};
-				// order matters scale, then rotate and then translate
-				world_matrix *= scale_matrix;
-				world_matrix *= rotatation_matrix_z;
-				world_matrix *= rotatation_matrix_y;
-				world_matrix *= rotatation_matrix_x;
-				world_matrix *= translation_matrix;
-				point = world_matrix.mult_vec4d(point);
-				point = view_matrix.mult_vec4d(point);
-				transformed_vertices.push_back(point);
-				
-				vector::Vector4d transformed_normal{ face_vtx_normals[counter] };
-				transformed_normal = world_matrix.get_inverse().get_transpose().mult_vec4d(transformed_normal);
-				transformed_normal = view_matrix.get_inverse().get_transpose().mult_vec4d(transformed_normal);
-				vector::Vector3d transformed_normal_vec3{ transformed_normal };
-				transformed_normal_vec3.normalize();
-				transformed_normals.emplace_back(transformed_normal_vec3);
-				counter++;
-			}
-
-			std::vector<vector::Vector3d> per_vertex_normals{ mesh_to_render.get_per_vertex_normals(transformed_vertices) };
-			if (backface_culling) {
-				// find the camera ray ie the vector between a point in the triangle and the camera origin
-				vector::Vector3d camera_ray{ origin - transformed_vertices[0] };
-
-				// calculate how aligned the camera ray is with the face normal (using dot product)
-				if (camera_ray.dot_product(per_vertex_normals[0]) < 0)
-				{
-					continue;
-				}
-			}
-
-			//clipping
-			//loop through each vertex and project the points of those faces
-			// convert the triangle points to ints as we'll project to screen space so we're dealing with x & y pixels which are in ints
-
-			// uvs don't need to be transformed
-			std::vector<vector::Vector2d<double>> face_vtx_uvs{
-				mesh_to_render.m_uvs[face.a_uv - 1],
-				mesh_to_render.m_uvs[face.b_uv - 1],
-				mesh_to_render.m_uvs[face.c_uv - 1]
-			};
-			
-			geo::Polygon poly{ transformed_vertices, transformed_normals, face_vtx_uvs };
-			poly.clip(clipping_planes);
-			std::vector<std::vector<vector::Vector4d>> clipped_vertices_array{};
-			std::vector<std::vector<vector::Vector2d<double>>> clipped_uvs_array{};
-			std::vector<std::vector<vector::Vector3d>> clipped_normals_array{};
-			poly.split_into_tris(clipped_vertices_array, clipped_uvs_array, clipped_normals_array);
-			
-			std::size_t clipped_entities_counter{ 0 };
-			for (const auto& clipped_vertex_array : clipped_vertices_array)
-			{
-				geo::Triangle<int> triangle_to_render{ };
-				double light_intensity{ -per_vertex_normals[0].dot_product(light.m_direction) };
-				triangle_to_render.m_light_intensity = light_intensity;
-				triangle_to_render.m_surface = mesh_to_render.m_surface;
-				// project the triangle
-				counter = 0;
-				for (const auto& vertex : clipped_vertex_array)
-				{
-					vector::Vector4d projected_point{ display::project_vec4d(projection_matrix, vertex) };
-					triangle_to_render.m_per_vtx_lt_intensity.push_back(-clipped_normals_array[clipped_entities_counter][counter].dot_product(light.m_direction));
-					triangle_to_render.m_points.push_back(vector::Vector2d<int>{static_cast<int>(projected_point.m_x), static_cast<int>(projected_point.m_y)});
-					triangle_to_render.m_points_z.push_back(projected_point.m_z);
-					triangle_to_render.m_points_w.push_back(projected_point.m_w);
-					triangle_to_render.m_uvs.push_back(clipped_uvs_array[clipped_entities_counter][counter]);
-					counter++;
-				}
-				if (render_face_center || render_normals)
-				{
-					vector::Vector3d face_center{ mesh_to_render.get_face_center(clipped_vertex_array) };
-					vector::Vector4d projected_center_point{ display::project_vec4d(projection_matrix, face_center) };
-
-					triangle_to_render.m_center.m_x = projected_center_point.m_x;
-					triangle_to_render.m_center.m_y = projected_center_point.m_y;
-					if (render_normals)
-					{
-						vector::Vector3d face_normal{ clipped_normals_array[clipped_entities_counter][0] };
-						// move the normal to the center of the face
-						face_normal.m_x += face_center.m_x;
-						face_normal.m_y += face_center.m_y;
-						face_normal.m_z += face_center.m_z;
-						face_normal.normalize();
-
-						vector::Vector4d projected_face_normal_point{ display::project_vec4d(projection_matrix, face_normal) };
-
-						triangle_to_render.m_face_normal.m_x = projected_face_normal_point.m_x;
-						triangle_to_render.m_face_normal.m_y = projected_face_normal_point.m_y;
-					}
-				}
-
-				triangles_to_render.push_back(triangle_to_render);
-				clipped_entities_counter++;
-			}
-		}
+		process_graphics_pipeline(mesh_to_render, projection_matrix, triangles_to_render, view_camera, clipping_planes, backface_culling, light, render_face_center, render_normals);
 	}
 }
 
